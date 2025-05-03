186a619e8321e8b470fafac448da6af8e6b322c5
dev-sufyaan <dev.sufyaan@gmail.com>
2025-05-03 17:10:42 +0530

    58

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,23 @@
 import { generateMetadata } from "@/functions";
 import { Metadata } from "next";
+import React from 'react';
 
 export const metadata: Metadata = generateMetadata({
   title: "Dashboard",
   description: "Access your personalized learning dashboard with Lumora AI",
   noIndex: true // We don't want search engines to index internal pages
 });
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function MainLayout({ children }: Props) {
+  return (
+    <>
+      {children}
+    </>
+  );
+}
 
 // Rest of the layout code 
